91ff71a839e4c13c8e32f6fe56e06d11a929a461
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-15 16:35:27 +0800

    fix

---
+++ 02_basic_calculator/10_as_casting/src/lib.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn u16_to_u32() {
+        let v: u32 = 47;
+        assert_eq!(47u16 as u32, v);
+    }
+
+    #[test]
+    fn u8_to_i8() {
+        #[allow(overflowing_literals)]
+        let x = { 255 as i8 };
+
+        let y: i8 = -1;
+
+        assert_eq!(x, y)
+    }
+
+    #[test]
+    fn bool_to_u8() {
+        let v: u8 = 1;
+        assert_eq!(true as u8, v);
+    }
+}
