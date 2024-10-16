d0950484d5ac0d3750e4ba62846b4fe91ae2f432
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-16 11:56:35 +0800

    fix

---
+++ 03_ticket_v1/01_struct/src/lib.rs
@@ -0,0 +1,35 @@
+struct Order {
+    price: u32,
+    quantity: u32,
+}
+
+impl Order {
+    fn is_available(&self) -> bool {
+        if self.price > 0 && self.quantity > 0 { true } else { false }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Order;
+
+    #[test]
+    fn test_is_available() {
+        let order = Order {
+            price: 100,
+            quantity: 10,
+        };
+
+        assert!(order.is_available());
+    }
+
+    #[test]
+    fn test_is_not_available() {
+        let order = Order {
+            price: 100,
+            quantity: 0,
+        };
+
+        assert!(!order.is_available());
+    }
+}
