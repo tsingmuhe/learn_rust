eedebc2b7710011208d843d804966ababf9b2f0c
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-16 12:23:20 +0800

    Update lib.rs

--- 03_ticket_v1/02_validation/src/lib.rs
+++ 03_ticket_v1/02_validation/src/lib.rs
@@ -40,32 +40,32 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "Title cannot be empty")]
-    fn test_title_cannot_be_empty() {
-        Ticket::new("".to_string(), valid_description(), valid_status());
-    }
-
-    #[test]
-    #[should_panic(expected = "Title cannot be longer than 50 bytes")]
-    fn test_title_cannot_be_longer_than_fifty_chars() {
-        Ticket::new(overly_long_title(), valid_description(), valid_status());
+    fn title_cannot_be_empty() {
+        Ticket::new("".into(), valid_description(), "To-Do".into());
     }
 
     #[test]
     #[should_panic(expected = "Description cannot be empty")]
-    fn test_description_cannot_be_empty() {
-        Ticket::new(valid_title(), "".to_string(), valid_status());
+    fn description_cannot_be_empty() {
+        Ticket::new(valid_title(), "".into(), "To-Do".into());
+    }
+
+    #[test]
+    #[should_panic(expected = "Title cannot be longer than 50 bytes")]
+    fn title_cannot_be_longer_than_fifty_chars() {
+        Ticket::new(overly_long_title(), valid_description(), "To-Do".into());
     }
 
     #[test]
     #[should_panic(expected = "Description cannot be longer than 500 bytes")]
-    fn test_description_cannot_be_longer_than_500_chars() {
-        Ticket::new(valid_title(), overly_long_description(), valid_status());
+    fn description_cannot_be_longer_than_500_chars() {
+        Ticket::new(valid_title(), overly_long_description(), "To-Do".into());
     }
 
     #[test]
     #[should_panic(expected = "Only `To-Do`, `In Progress`, and `Done` statuses are allowed")]
     fn status_must_be_valid() {
-        Ticket::new(valid_title(), valid_description(), "sunchp".to_string());
+        Ticket::new(valid_title(), valid_description(), "Funny".into());
     }
 
     #[test]
