d4e573fff448ed55e15363062c34ea539211a19b
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-17 17:54:41 +0800

    fix

---
+++ 03_ticket_v1/10_references_in_memory/src/lib.rs
@@ -0,0 +1,29 @@
+pub struct Ticket {
+    title: String,
+    description: String,
+    status: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Ticket;
+    use std::mem::size_of;
+
+    #[test]
+    fn u16_ref_size() {
+        assert_eq!(size_of::<u16>(), 2);
+        assert_eq!(size_of::<&u16>(), 8);
+    }
+
+    #[test]
+    fn u64_mut_ref_size() {
+        assert_eq!(size_of::<u64>(), 8);
+        assert_eq!(size_of::<&mut u64>(), 8);
+    }
+
+    #[test]
+    fn ticket_ref_size() {
+        assert_eq!(size_of::<Ticket>(), 72);
+        assert_eq!(size_of::<&Ticket>(), 8);
+    }
+}
