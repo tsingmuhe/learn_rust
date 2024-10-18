bc249a711abd433f757965492c21b991cf78172c
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-18 17:35:54 +0800

    fix

---
+++ 04_traits/07_deref/src/lib.rs
@@ -0,0 +1,30 @@
+pub struct Ticket {
+    title: String,
+    description: String,
+}
+
+impl Ticket {
+    pub fn title(&self) -> &str {
+        self.title.trim()
+    }
+
+    pub fn description(&self) -> &str {
+        self.description.trim()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normalization() {
+        let ticket = Ticket {
+            title: "   A title ".to_string(),
+            description: " A description   ".to_string(),
+        };
+
+        assert_eq!("A title", ticket.title());
+        assert_eq!("A description", ticket.description());
+    }
+}
