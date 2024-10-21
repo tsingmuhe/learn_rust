fcff90e99174705970322cb1fb9bff05f1db42d6
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-21 20:02:38 +0800

    fix

---
+++ 04_traits/14_outro/src/lib.rs
@@ -0,0 +1,75 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self { value: u16::from(*value) }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self { value: value.into() }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self { value: u16::from(*value) }
+    }
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: SaturatingU16) -> Self::Output {
+        Self { value: self.value.saturating_add(rhs.value) }
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        Self { value: self.value.saturating_add(rhs.value) }
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        Self { value: self.value.saturating_add(rhs) }
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        Self { value: self.value.saturating_add(*rhs) }
+    }
+}
+
+
+impl PartialEq for SaturatingU16 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
