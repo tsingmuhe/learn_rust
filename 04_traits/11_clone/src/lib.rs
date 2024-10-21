695a7ff8187b746ff5ace22c6fac609e2c1b22d1
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-21 18:02:52 +0800

    Update lib.rs

--- 04_traits/11_clone/src/lib.rs
+++ 04_traits/11_clone/src/lib.rs
@@ -1,23 +1,25 @@
+#[derive(Clone)]
 pub struct Ticket {
     pub title: String,
     pub description: String,
     pub status: String,
 }
+
 pub struct Summary {
     pub title: String,
     pub status: String,
 }
 
 impl Ticket {
-    pub fn summary(&self) -> Summary {
+    pub fn summary(self) -> Summary {
         Summary {
-            title: self.title.clone(),
-            status: self.status.clone(),
+            title: self.title,
+            status: self.status,
         }
     }
 }
 
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    let summary = ticket.summary();
+    let summary = ticket.clone().summary();
     (ticket, summary)
 }
