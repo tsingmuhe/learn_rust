5c89b556d7c8c0b6abed04f6204506bd04054ce2
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-21 21:40:26 +0800

    Update lib.rs

--- 05_ticket_v2/03_variants_with_data/src/lib.rs
+++ 05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -35,8 +35,8 @@ impl Ticket {
     }
 
     pub fn assigned_to(&self) -> &str {
-        match self.status {
-            Status::InProgress { ref assigned_to } => {
+        match &self.status {
+            Status::InProgress { assigned_to } => {
                 assigned_to
             }
             _ => panic!("Only `In-Progress` tickets can be assigned to someone"),
