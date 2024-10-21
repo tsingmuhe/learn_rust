e4488351847cb80cd8529f1c4fcb971b38f2333d
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-21 20:39:04 +0800

    fix

---
+++ 05_ticket_v2/01_enum/src/lib.rs
@@ -0,0 +1,133 @@
+#[derive(Debug, PartialEq)]
+struct Ticket {
+    title: String,
+    description: String,
+    status: Status,
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Status {
+    ToDo,
+    InProgress,
+    Done,
+}
+
+impl Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Ticket {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+
+        Ticket { title, description, status }
+    }
+
+    pub fn title(&self) -> &String {
+        &self.title
+    }
+
+    pub fn description(&self) -> &String {
+        &self.description
+    }
+
+    pub fn status(&self) -> Status {
+        self.status
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_partial_eq() {
+        let title = "valid_title".to_string();
+        let description = "valid_description".to_string();
+
+        let ticket1 = Ticket {
+            title: title.clone(),
+            description: description.clone(),
+            status: Status::ToDo,
+        };
+
+        let ticket2 = Ticket {
+            title: title.clone(),
+            description: description.clone(),
+            status: Status::ToDo,
+        };
+
+        assert_eq!(ticket1, ticket2);
+    }
+
+    #[test]
+    fn test_description_not_matching() {
+        let title = "valid_title".to_string();
+        let status = Status::ToDo;
+
+        let ticket1 = Ticket {
+            title: title.clone(),
+            description: "description".to_string(),
+            status,
+        };
+
+        let ticket2 = Ticket {
+            title: title.clone(),
+            description: "description2".to_string(),
+            status,
+        };
+
+        assert_ne!(ticket1, ticket2);
+    }
+
+    #[test]
+    fn test_title_not_matching() {
+        let description = "valid_description".to_string();
+        let status = Status::InProgress;
+
+        let ticket1 = Ticket {
+            title: "title".to_string(),
+            description: description.clone(),
+            status,
+        };
+
+        let ticket2 = Ticket {
+            title: "title2".to_string(),
+            description: description.clone(),
+            status,
+        };
+
+        assert_ne!(ticket1, ticket2);
+    }
+
+    #[test]
+    fn test_status_not_matching() {
+        let title = "valid_title".to_string();
+        let description = "valid_description".to_string();
+
+        let ticket1 = Ticket {
+            title: title.clone(),
+            description: description.clone(),
+            status: Status::InProgress,
+        };
+
+        let ticket2 = Ticket {
+            title: title.clone(),
+            description: description.clone(),
+            status: Status::Done,
+        };
+
+        assert_ne!(ticket1, ticket2);
+    }
+}
