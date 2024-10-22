d656f9dd47705801d21f9052e27b1079e9616d80
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-22 15:12:09 +0800

    fix

---
+++ 05_ticket_v2/04_if_let/src/lib.rs
@@ -0,0 +1,48 @@
+enum Shape {
+    Circle { radius: f64 },
+    Square { border: f64 },
+    Rectangle { width: f64, height: f64 },
+}
+
+
+impl Shape {
+    pub fn radius(&self) -> f64 {
+        // if let Shape::Circle { radius } = self {
+        //     *radius
+        // } else {
+        //     panic!("This should not happen")
+        // }
+
+        let Shape::Circle { radius } = self else {
+            panic!("Circle shape has no radius");
+        };
+
+        *radius
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_circle() {
+        let _ = Shape::Circle { radius: 1.0 }.radius();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_square() {
+        let _ = Shape::Square { border: 1.0 }.radius();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_rectangle() {
+        let _ = Shape::Rectangle {
+            width: 1.0,
+            height: 2.0,
+        }
+            .radius();
+    }
+}
