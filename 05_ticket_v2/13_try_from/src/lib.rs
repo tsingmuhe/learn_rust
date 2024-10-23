db24eb1d1d11ffde9f9da2336a77c20aef869b1b
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-23 11:11:19 +0800

    fix

---
+++ 05_ticket_v2/13_try_from/src/lib.rs
@@ -0,0 +1,70 @@
+#[derive(Debug, PartialEq, Clone)]
+enum Status {
+    ToDo,
+    InProgress,
+    Done,
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("{value} is not a valid status")]
+struct ParseStatusError {
+    value: String,
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => { Ok(Status::ToDo) }
+            "inprogress" => { Ok(Status::InProgress) }
+            "done" => { Ok(Status::Done) }
+            _ => Err(ParseStatusError { value: value.to_string() }),
+        }
+    }
+}
+
+impl TryFrom<String> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Status::try_from(value.as_str())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_try_from_string() {
+        let status = Status::try_from("ToDO".to_string()).unwrap();
+        assert_eq!(status, Status::ToDo);
+
+        let status = Status::try_from("inproGress".to_string()).unwrap();
+        assert_eq!(status, Status::InProgress);
+
+        let status = Status::try_from("Done".to_string()).unwrap();
+        assert_eq!(status, Status::Done);
+    }
+
+    #[test]
+    fn test_try_from_str() {
+        let status = Status::try_from("todo").unwrap();
+        assert_eq!(status, Status::ToDo);
+
+        let status = Status::try_from("inprogress").unwrap();
+        assert_eq!(status, Status::InProgress);
+
+        let status = Status::try_from("done").unwrap();
+        assert_eq!(status, Status::Done);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_status() {
+        let status = Status::try_from("todo1").unwrap();
+    }
+}
+
