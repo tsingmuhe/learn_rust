aa40cdc8e1fe759eacf87d204205ecc17cbdc85a
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-23 14:06:29 +0800

    fix

---
+++ 06_ticket_management/02_vec/src/lib.rs
@@ -0,0 +1,41 @@
+pub fn fibonacci(n: u32) -> u32 {
+    let n = n as usize;
+
+    let mut v = vec![0, 1];
+
+    for i in 2..=n {
+        v.push(v[i - 1] + v[i - 2]);
+    }
+
+    v[n]
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::fibonacci;
+
+    #[test]
+    fn first() {
+        assert_eq!(fibonacci(0), 0);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(fibonacci(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(fibonacci(2), 1);
+    }
+
+    #[test]
+    fn tenth() {
+        assert_eq!(fibonacci(10), 55);
+    }
+
+    #[test]
+    fn thirtieth() {
+        assert_eq!(fibonacci(30), 832040);
+    }
+}
