e42bd74333ffc7cb811bc7286f923043195741b8
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-24 17:21:23 +0800

    fix

---
+++ 06_ticket_management/09_impl_trait_2/src/lib.rs
@@ -0,0 +1,72 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct TicketTitle(String);
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TicketDescription(String);
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Status {
+    ToDo,
+    InProgress,
+    Done,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Ticket {
+    pub title: TicketTitle,
+    pub description: TicketDescription,
+    pub status: Status,
+}
+
+#[derive(Clone)]
+pub struct TicketStore {
+    tickets: Vec<Ticket>,
+}
+
+impl TicketStore {
+    pub fn new() -> Self {
+        Self {
+            tickets: Vec::new(),
+        }
+    }
+
+    pub fn add_ticket<T: Into<Ticket>>(&mut self, ticket: T) {
+        self.tickets.push(ticket.into());
+    }
+
+    pub fn in_progress(&self) -> impl Iterator<Item=&Ticket> {
+        self.tickets
+            .iter()
+            .filter(|&t| t.status == Status::InProgress)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TicketDraft {
+        pub title: TicketTitle,
+        pub description: TicketDescription,
+    }
+
+    impl From<TicketDraft> for Ticket {
+        fn from(draft: TicketDraft) -> Self {
+            Self {
+                title: draft.title,
+                description: draft.description,
+                status: Status::ToDo,
+            }
+        }
+    }
+
+    #[test]
+    fn generic_add() {
+        let mut store = TicketStore::new();
+        // This won't compile if `add_ticket` uses `impl Trait` syntax in argument position.
+        store.add_ticket::<TicketDraft>(TicketDraft {
+            title: TicketTitle("ticket_title".to_string()),
+            description: TicketDescription("ticket_description".to_string()),
+        });
+    }
+}
