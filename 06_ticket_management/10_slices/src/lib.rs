5b8b2fef73434361a886780547ebf2bcaeb7ec94
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-25 02:18:25 +0800

    fix

---
+++ 06_ticket_management/10_slices/src/lib.rs
@@ -0,0 +1,32 @@
+fn sum(slice: &[i32]) -> i32 {
+    slice.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let v = vec![];
+        assert_eq!(sum(&v), 0);
+    }
+
+    #[test]
+    fn one_element() {
+        let v = vec![1];
+        assert_eq!(sum(&v), 1);
+    }
+
+    #[test]
+    fn multiple_elements() {
+        let v = vec![1, 2, 3, 4, 5];
+        assert_eq!(sum(&v), 15);
+    }
+
+    #[test]
+    fn array_slice() {
+        let v = [1, 2, 3, 4, 5];
+        assert_eq!(sum(&v), 15);
+    }
+}
---
+++ 06_ticket_management/11_mutable_slices/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn squared(slice: &mut [i32]) {
+    for i in slice {
+        let a = *i;
+        *i = a * a
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let mut s = vec![];
+        squared(&mut s);
+        assert_eq!(s, vec![]);
+    }
+
+    #[test]
+    fn one() {
+        let mut s = [2];
+        squared(&mut s);
+        assert_eq!(s, [4]);
+    }
+
+    #[test]
+    fn multiple() {
+        let mut s = vec![2, 4];
+        squared(&mut s);
+        assert_eq!(s, vec![4, 16]);
+    }
+}
