c9979d50fe0689c94b6241e76613b3c2fefaf7f4
sunchangpeng <sunchangpeng@bytedance.com>
2024-10-27 00:11:44 +0800

    fix

---
+++ 07_threads/02_static/src/lib.rs
@@ -0,0 +1,47 @@
+use std::thread;
+
+pub fn sum(slice: &'static [i32]) -> i32 {
+    let mid = slice.len() / 2;
+    let (v1, v2) = slice.split_at(mid);
+
+    let t1 = thread::spawn(|| v1.iter().sum::<i32>());
+    let t2 = thread::spawn(|| v2.iter().sum::<i32>());
+
+    t1.join().unwrap() + t2.join().unwrap()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        static ARRAY: [i32; 0] = [];
+        assert_eq!(sum(&ARRAY), 0);
+    }
+
+    #[test]
+    fn one() {
+        static ARRAY: [i32; 1] = [1];
+        assert_eq!(sum(&ARRAY), 1);
+    }
+
+    #[test]
+    fn five() {
+        static ARRAY: [i32; 5] = [1, 2, 3, 4, 5];
+        assert_eq!(sum(&ARRAY), 15);
+    }
+
+    #[test]
+    fn nine() {
+        static ARRAY: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        assert_eq!(sum(&ARRAY), 45);
+    }
+
+    #[test]
+    fn ten() {
+        static ARRAY: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        assert_eq!(sum(&ARRAY), 55);
+    }
+}
--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 new_crate:
-	cargo new 07_threads/01_threads --lib --name ex_07_threads_01_threads
+	cargo new 07_threads/02_static --lib --name ex_07_threads_02_static
